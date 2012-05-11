acf9d679c085e7452ca126f65bbf87c257848d51
wittydiary <wittydiary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-11 01:42:24 +0000

    git-svn-id: https://plugins.svn.wordpress.org/recent-tweets-widget-enhanced/trunk@542658 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Recent Tweets Enhanced.php
@@ -0,0 +1,119 @@
+<?php
+/*
+Plugin Name: Recent Tweets Enhanced Widget 2.3
+Plugin URI: http://www.wittydiary.com
+Description: Display your most recent Tweets in a sidebar widget easily. Now, you can show from 1 up to 20 Tweets! Blends in with your template automatically. To install this plugin, activate it and then go to Appearance > Widgets. Drag the widget 'Recent Tweets Enhanced' to the sidebar. To change the settings, simply fill in required information on the widget itself.
+Version: 2.3
+Author: WittyBlogger
+Author URL: http://www.wittydiary.com
+*/
+
+add_action("widgets_init", array('Recent_Tweets_Enhanced', 'register'));
+register_activation_hook( __FILE__, array('Recent_Tweets_Enhanced', 'activate'));
+register_deactivation_hook( __FILE__, array('Recent_Tweets_Enhanced', 'deactivate'));
+class Recent_Tweets_Enhanced {
+  function activate(){
+    $data = array('titletweets' => 'Recent Tweets', 'option1' => '', 'option2' => '3', 'option3' => 'No', 'option4' => 'No');
+    if ( ! get_option('recent_tweets_enhanced')){
+      add_option('recent_tweets_enhanced' , $data);
+    } else {
+      update_option('recent_tweets_enhanced' , $data);
+    }
+  }
+  
+ function control(){
+  $data = get_option('recent_tweets_enhanced');
+  ?>
+  
+  <!-- SETTINGS -->
+  
+    <p><label>Title: <input name="recent_tweets_titletweets"
+type="text" value="<?php echo $data['titletweets']; ?>" /></label></p>
+  <p><label>Your Twitter Username:<br />@<input name="recent_tweets_option1"
+type="text" value="<?php echo $data['option1']; ?>" /></label></p>
+  <p><label>Choose # of Tweets to Display:</label> 
+  <select name="recent_tweets_option2">
+  <option <?php if ($data['option2'] == 1){echo 'selected="selected"';} ?>>1</option>
+  <option <?php if ($data['option2'] == 2){echo 'selected="selected"';} ?>>2</option>
+  <option <?php if ($data['option2'] == 3){echo 'selected="selected"';} ?>>3</option>
+  <option <?php if ($data['option2'] == 4){echo 'selected="selected"';} ?>>4</option>
+  <option <?php if ($data['option2'] == 5){echo 'selected="selected"';} ?>>5</option>
+  <option <?php if ($data['option2'] == 6){echo 'selected="selected"';} ?>>6</option>
+  <option <?php if ($data['option2'] == 7){echo 'selected="selected"';} ?>>7</option>
+  <option <?php if ($data['option2'] == 8){echo 'selected="selected"';} ?>>8</option>
+  <option <?php if ($data['option2'] == 9){echo 'selected="selected"';} ?>>9</option>
+  <option <?php if ($data['option2'] == 10){echo 'selected="selected"';} ?>>10</option>
+  <option <?php if ($data['option2'] == 11){echo 'selected="selected"';} ?>>11</option>
+  <option <?php if ($data['option2'] == 12){echo 'selected="selected"';} ?>>12</option>
+  <option <?php if ($data['option2'] == 13){echo 'selected="selected"';} ?>>13</option>
+  <option <?php if ($data['option2'] == 14){echo 'selected="selected"';} ?>>14</option>
+  <option <?php if ($data['option2'] == 15){echo 'selected="selected"';} ?>>15</option>
+  <option <?php if ($data['option2'] == 16){echo 'selected="selected"';} ?>>16</option>
+  <option <?php if ($data['option2'] == 17){echo 'selected="selected"';} ?>>17</option>
+  <option <?php if ($data['option2'] == 18){echo 'selected="selected"';} ?>>18</option>
+  <option <?php if ($data['option2'] == 19){echo 'selected="selected"';} ?>>19</option>
+  <option <?php if ($data['option2'] == 20){echo 'selected="selected"';} ?>>20</option>
+</select></p>
+<p><label>Allow Visitors To Follow You?</label> 
+  <select name="recent_tweets_option3">
+  <option <?php if ($data['option3'] == "Yes"){echo 'selected="selected"';} ?>>Yes</option>
+  <option <?php if ($data['option3'] == "No"){echo 'selected="selected"';} ?>>No</option>
+</select></p>
+<p><label>Credit Us? (We'll Love You!)</label>
+  <select name="recent_tweets_option4">
+  <option <?php if ($data['option4'] == "Yes"){echo 'selected="selected"';} ?>>Yes</option>
+  <option <?php if ($data['option4'] == "No"){echo 'selected="selected"';} ?>>No</option>
+</select></p>
+
+  <?php
+   if (isset($_POST['recent_tweets_option1'])){
+   	$data['titletweets'] = attribute_escape($_POST['recent_tweets_titletweets']);
+    $data['option1'] = attribute_escape($_POST['recent_tweets_option1']);
+    $data['option2'] = attribute_escape($_POST['recent_tweets_option2']);
+    $data['option3'] = attribute_escape($_POST['recent_tweets_option3']);
+    $data['option4'] = attribute_escape($_POST['recent_tweets_option4']);
+    update_option('recent_tweets_enhanced', $data);
+  }
+}
+
+
+  function deactivate(){
+    delete_option('recent_tweets_enhanced');
+  }
+  function widget($args){
+  	$data = get_option('recent_tweets_enhanced');
+    echo $args['before_widget'];
+    echo $args['before_title'] . $data['titletweets'] . $args['after_title'];
+    echo '<style type="text/css">
+#twitter_update_list li {
+list-style-type: none;
+padding-top: 10px;
+}</style>
+<div id="twitter_update_list">
+<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"> </script>
+<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/'.$data['option1'].'.json?callback=twitterCallback2&count='.$data['option2'].'"></script></div><br />';
+
+$firstletter = $data['option1'];
+if ($firstletter[0] == "a" || $firstletter[0] == "i" || $firstletter[0] == "m" || $firstletter[0] == "c" || $firstletter[0] == "j" || $firstletter[0] == "s" || $firstletter[0] == "e" || $firstletter[0] == "p" || $firstletter[0] == "g"){
+$thelink = "Make Money Online";
+} else {
+$thelink = "Make Money Online";
+}
+
+	if ($data['option3'] == "Yes"){
+echo '<a href="http://twitter.com/'.$data['option1'].'">Follow @'.$data['option1'].'</a><br />';} else {}
+		
+if ($data['option4'] == "Yes"){
+echo 'Powered By: <a href="http://wittydiary.com/">'.$thelink.'</a>';} else {}
+
+echo $args['after_widget'];
+  }
+function register(){
+    register_sidebar_widget('Recent Tweets Enhanced', array('Recent_Tweets_Enhanced', 'widget'));
+    register_widget_control('Recent Tweets Enhanced', array('Recent_Tweets_Enhanced', 'control'));
+  }
+}
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,76 @@
+===Recent Tweets ===
+Contributors: Recent Tweets Enhanced
+Tags: twitter, recent, tweets, widget, sidebar, plugin
+Requires at least: 2.0.0
+Tested up to: 3.2.1
+Stable tag: 2.3
+
+Display up to 20 of your most recent tweets easily with zero hassle in your sidebar as a widget. Options for following you and others included.
+
+== Description ==
+
+This super-simple plugin allows you to easily display the most recent Tweets from any Twitter account as a sidebar widget on your blog. 
+
+= Why Use Recent Tweets? =
+
+* **Easy, Bulletproof Functionality -** Just enter your Twitter username and go. It's that simple. 
+* **Fast Customization -** Changing the options, such as the number of Tweets to display, is a snap. 
+* **Get More Followers -** Promote your Twitter account with a "follow me" link under your most recent Tweets.
+
+= Support: =
+*Want to see more features? Need Support? Visit the official plugin page here: http://www.zzzprofits.com/plugin/recent-tweets/* and leave a comment. 
+
+
+== Installation ==
+
+1. Upload the file `recent-tweets.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to Appearance > Widgets and drag 'Recent Tweets' to your sidebar
+4. Update the settings in the widget: Username, # of Tweets to show, etc.
+
+
+== Frequently Asked Questions ==
+
+= What do I type under "Title"? =
+"Title" refers to your widget's heading. If you fill in "My Recent Tweets", it will show up as "My Recent Tweets" for your title of the sidebar widget above your tweets.
+
+= What do I type under "Your Twitter Username"? =
+You need to add in your Twitter username here. This plugin will then automatically pull out your most recent tweets straight from the Twitter feed.
+
+= What do I do for "Choose # of Tweets to Display"? =
+The recommended size is 5 tweets, but you can choose as many as you like if you have a long blog page. We've added display for up to 20 tweets, due to popular demand.
+
+= What do I do for "Display "Follow @[yourtwitterid]"?" =
+If you choose Yes, the link (Follow@yourtwitterid) will appear under your tweets. This will make it more convenient for people to follow you on Twitter with just one click.
+
+== Screenshots ==
+
+1. Simple settings from within the widget itself.
+
+2. An example implementation. 
+
+== Changelog ==
+
+= 0.1 =
+Beta version in experiment
+
+= 1.0 =
+First stable release with bug fixes.
+
+= 2.0 =
+More bug fixes.
+
+= 2.3 =
+Increased number of options and flexibility.
+
+
+== Upgrade Notice ==
+
+Version 0.1 - Initial Release
+
+Version 1.0 - Bug Fixes and Stable Release
+
+Version 2.0 - Bug Fixes
+
+Version 2.3 - More features implemented.
+
